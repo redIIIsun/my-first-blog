5e0223017c31dc1cfc2108d049bfc1d5133990d7
Evelyna Rashchuk <evelyna7@gmail.com>
2021-09-09 10:55:37 +0300

    static in html

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+"""my site url Configuration
 
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
