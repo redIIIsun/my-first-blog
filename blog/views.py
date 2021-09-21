85460c0452193024efb97c039832295e88d36306
Evelyna Rashchuk <evelyna7@gmail.com>
2021-09-21 10:05:44 +0300

    "Extend application"

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return redirect('post_detail', pk=post.pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
